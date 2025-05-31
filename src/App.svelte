47c3275a857eda76d21f07b0168b2718e5fe2306
rza628 <ryanzhang2468@gmail.com>
2025-05-30 23:33:46 -0400

    yayyy

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
@@ -14,18 +14,20 @@
     </a>
   </div>
   <h1>Vite + Svelte</h1>
-
+  <h1>ANIME GRID YAYYYY</h1>
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
