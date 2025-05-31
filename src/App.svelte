d30d9d02b4dab634d506bdaef028c25d9d52a534
LavdrimIslamiTU <lavdrim.islami280@gmail.com>
2025-05-31 00:32:46 -0400

    Grid and title header

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,39 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import Grid from "./lib/Grid.svelte";
 </script>
+
+
+<header>
+    <h1 class = "Title">Anime Grid</h1>
+</header>
+
+<footer>
+  <h1 class = "Footer">Ry Ly</h1>
+</footer>
+
 
 <main>
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <Grid/>
   </div>
-  <h1>Vite + Svelte</h1>
-  <h1>ANIME GRID YAYYYY</h1>
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-
-
 </main>
 
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .Title{
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    text-align: center;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .Footer{
+    position: fixed;
+    left: 0;
+    bottom:0;
+    width: 100%;
+    text-align: center;
+    font-size: small;
   }
 </style>
+
---
+++ src/lib/Grid.svelte
@@ -0,0 +1,24 @@
+<div class="grid">
+  <div class="cell1"></div>
+  <div class="cell2"></div>
+  <div class="cell3"></div>
+  <div class="cell4"></div>
+  <div class="cell5"></div>
+  <div class="cell6"></div>
+  <div class="cell7"></div>
+  <div class="cell8"></div>
+  <div class="cell9"></div>
+</div>
+
+<style>
+.grid {
+  display: grid;
+  gap: 1px;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+div > div {
+  padding: 100px;
+  background-color: #ccc;
+}
+</style>
