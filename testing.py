eed361917a363b2ab9402202053872d5ff37b293
rza628 <ryanzhang2468@gmail.com>
2025-08-03 00:05:13 -0400

    added json files for studios, genres(epxlicit, themes, demographics as well), and top 1000 animes

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import time
+import json
 sys.stdout.reconfigure(encoding='utf-8')
 encoding = 'utf-8'
 def malapi():
@@ -33,57 +34,50 @@ def genres():
             return data
     except:
         return None
+    
+def getTitleSynonyms(id):
+    url = f'https://api.jikan.moe/v4/anime/{id}'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if data['data']['title_synonyms']:
+                return data['data']['title_synonyms']
+            else:
+                return []
+    except:
+        return None
+
+def genres():
+    stats = {'genres':[], 'explicit_genres':[], 'themes':[],"demographics":[]}
+
+    for enum in stats.keys():
+
+        url = f'https://api.jikan.moe/v4/genres/anime?filter={enum}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            data = sorted(data['data'], key= lambda x: x['count'], reverse=True)
+            stats[enum] = data
+    return stats
+
+def getTop(n):
+    topN = {'top1000':[]}
+    for i in range(1, n + 1):
+        url = f'https://api.jikan.moe/v4/top/anime?page={i}'
+        response = requests.get(url)
+        data = response.json()
+        data = data['data']
+        for entry in data:
+            name, id = entry['title'], entry['mal_id']
+            topN['top1000'].append({"name":name, "id":id})
+        time.sleep(0.75)
+    return json.dumps(topN, indent=2)
+
  
-data = genres()
-genrelist = []
-for genre in data['data']:
-    genrelist.append({'genre':genre['name']})
-print(genrelist)
+file = open('top1000.json', 'r', encoding=encoding)
 
-""" data = fpl_api()['data'][1]
- 
-print(data['title']) """
-
-""" file  = open('anime_titles.txt', 'w', encoding='utf-8') """
-""" file = open("new_anime_titles_delayed_call.csv", 'w', encoding='utf-8')
-data = malapi()
-numPages = int(data['pagination']['last_visible_page'])
-for i in range(1, 1153, 1):
-    page = apiByPage(i)
-    if (page):
-        pageData = page['data']
-        for anime in pageData:
-            string = f'{anime['title']}, {anime['mal_id']}\n'
-            file.write(string)
-    if (i % 3 == 0):
-        time.sleep(1)
-print('done') """
-""" 
-file = open('anime_titles.txt', 'r', encoding='utf-8')
-file = file.read().strip().split('\n')
-newfile = open('anime_titles_nodupes.txt', 'w', encoding='utf-8')
-setNoDupe = set()
-for pair in file:
-    if pair not in setNoDupe:
-        setNoDupe.add(pair)
-        newfile.write(pair + '\n')
-print('done') """
-""" 
-l1 = open('./anime_titles_nodupes.csv', 'r', encoding='utf-8').read().strip().split('\n')
-l2 = open('./new_anime_titles_delayed_call.csv', 'r', encoding='utf-8').read().strip().split('\n')
-l3 = open('./new_anime_titles.csv', 'r', encoding='utf-8').read().strip().split('\n')
-
-file = open('combined_anime_titles.csv', 'w', encoding='utf-8')
-check = set([])
-
-for list in [l1, l2, l3]:
-    for pair in list:
-        if pair not in check:
-            check.add(pair)
-            file.write(pair + '\n')
-        else:
-            continue """
-
+print(len(json.load(file)['top1000']))
 
 
 
