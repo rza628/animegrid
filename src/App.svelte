ae272e1e32351c8c879dc221fffe745b8eb743bc
rza628 <ryanzhang2468@gmail.com>
2025-12-02 22:42:56 -0500

    added meta tag

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,10 @@
 </footer> -->
 <head>
   <title>AnimeGrid</title>
+  <meta
+    name="description"
+    content="Test your anime knowledge by guessing on a daily randomized grid of categories!"
+  />
 </head>
 
 <div>
